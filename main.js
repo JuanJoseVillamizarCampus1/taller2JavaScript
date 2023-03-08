cda5d211f44f7b90dc4d326ebf26ef8bc818cf39
JuanJoseVillamizarCampus1 <juanjvs1409@gmail.com>
2023-03-07 23:02:27 -0500

    feat

--- main.js
+++ main.js
@@ -67,3 +67,77 @@ else if (edad2>edad3&&edad2>edad1){
 else{
     alert(`La persona de mayor edad es: ${name3}`)
 }
+/*
+5. Construir el algoritmo que lea por teclado dos números,
+si el primero es mayor al segundo informar su suma y
+diferencia, en caso contrario, informar el producto y la
+división del primero respecto al segundo.*/
+
+let numero1 = Number(prompt("Ingrese un numero: "));
+let numero2 = Number(prompt("Ingrese otro numero: "));
+let suma;
+
+if (numero1>numero2){
+    suma = numero2+numero2;
+    resta = numero1-numero2;
+    alert(`La suma de los dos numeros es ${suma}, y la diferencia es ${resta}`)
+}
+else {
+    multi= numero1*numero2;
+    division =numero1/2;
+    alert(`La MUltiplicacion de los dos numeros es ${multi}, y la diferencia es ${division}`)
+}
+
+/*
+6. Construir el algoritmo en Javascript para un programa
+para cualquier cantidad de estudiantes que lea el nombre,
+el sexo y la nota definitiva y halle al estudiante con la mayor
+nota y al estudiante con la menor nota y cuantos eran
+hombres y cuantos mujeres.
+*/
+
+let cantidadDeEstudiantes = parseInt(prompt("Ingrese la cantidad de estudiantes"));
+let mujeres=0;
+let hombres=0;
+let notaMasAlta;
+let notaMasBaja;
+for (let i = 1 ; i <= cantidadDeEstudiantes; i++){
+    let name = prompt("Digite el nombre del estudiante").toUpperCase();
+    let sexo = prompt("Para registrar una mujer digite la letra M y para un hombre digite la letra H ").toUpperCase();
+    let notas = parseFloat(prompt("Digite la nota definitiva del estudiante"));
+
+
+    if (sexo === "M"){
+        mujeres++;
+    }
+    else if(sexo === "H"){
+        hombres++;
+    }
+    else{
+        alert("Digite un valor correcto para hombre o mujer");
+    }
+    if (notaMasAlta==undefined){
+        notaMasAlta = { name,sexo,notas};
+    }
+        else if(notas>notaMasAlta.notas){
+            name=notaMasAlta.name;
+            sexo=notaMasAlta.sexo;
+            notas=notaMasAlta.notas;
+        
+        }
+    if (notaMasBaja==undefined){
+            notaMasBaja = { name,sexo,notas};
+        }
+        else if(notas<notaMasBaja.notas){
+                name=notaMasBaja.name;
+                sexo=notaMasBaja.sexo;
+                notas=notaMasBaja.notas;
+            
+        }    
+   
+}
+
+console.log(`el total de hombres es ${hombres}`);
+console.log(`el total de mujeres es ${mujeres}`);
+console.log(`La nota mas alta fue ${notaMasAlta.notas}`)
+console.log(`La nota mas baja fue ${notaMasBaja.notas}`)
